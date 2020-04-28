713cd99edea064a60fa9c86592d501efcd8ee026
Jameson Welch <jamesonb.welch@gmail.com>
2020-04-27 19:14:33 -0700

    Initial upload

---
+++ monday.py
@@ -0,0 +1,23 @@
+# monday.py
+
+def archive_contract(*args, **kwargs):
+    """Final tear down of completed contract"""
+    loc = home_server
+    pass
+
+def instantiate_new_conctract():
+    """ Create folders and github repository for new contract """
+    pass
+
+def appointment_recall():
+    """ For appointment level 1, vocalize reminder """
+    pass
+
+def initialize_development_environment():
+    pass
+
+def send_file_to_home_server():
+    pass
+
+def update_remote_repository():
+    pass
