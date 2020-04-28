4a5bddb7e1f189560a9a6695445fa823b8711f67
JamesonWelch <jamesonb.welch@gmail.com>
2020-04-28 07:25:23 -0700

    Added shutdown protocols

--- monday.py
+++ monday.py
@@ -8,6 +8,7 @@ import os
 import time
 from time import ctime
 import pyttsx3
+import subprocess
 
 
 def there_exists(terms):
@@ -15,35 +16,43 @@ def there_exists(terms):
         if term in voice_data:
             return True
 
-def engine_speak(text):
-    text = str(text)
-    engine.say(text)
-    engine.runAndWait()
+# def monday_speak(text):
+#     text = str(text)
+#     engine.say(text)
+#     engine.runAndWait()
 
 r = sr.Recognizer()
 
 def record_audio(ask=''):
     with sr.Microphone() as source:
-        if ask:
-            engine_speak(ask)
+        r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
         print('Done Listening')
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
         except sr.UnknownValueError:
-            engine_speak('I did not get that')
+            monday_speak('I did not get that')
         except sr.RequestError:
-            engine_speak('I can\'t connect to my remote speech recognition servers')
+            monday_speak('I can\'t connect to my remote speech recognition servers')
+        except sr.WaitTimeoutError:
+            monday_speak('I\'m not hearing anything.')
         print('>>', voice_data.lower())
         return voice_data.lower()
+
 def monday_speak(audio_string):
     audio_string = str(audio_string)
     tts = gTTS(text=audio_string, lang='en')
+    r = random.randint(1,20000000)
     audio_file = 'audio-' + str(r) + '.mp3'
     tts.save(audio_file)
     playsound.playsound(audio_file)
     os.remove(audio_file)
+
+# def say(text):
+#     subprocess.call(['say', text])
+# say('hello')
+
 
 def respond(voice_data):
     if there_exists(['hey','hi','hello']):
@@ -51,9 +60,9 @@ def respond(voice_data):
         greet = greetings[random.randint(0, len(greetings)-1)]
         monday_speak(greet)
     if there_exists(["what is your name","what's your name","tell me your name"]):
-        engine_speak("I am Monday")
+        monday_speak("I am Monday")
     if there_exists(["how are you","how are you doing"]):
-        engine_speak("I exist in 1's and 0's. You do the math. The math is binary - that is, base 2, not base 10, by the way")
+        monday_speak("I exist in 1's and 0's. You do the math. The math is binary - that is, base 2, not base 10, by the way")
     if there_exists(["what's the time","tell me the time","what time is it"]):
         time = ctime().split(" ")[3].split(":")[0:2]
         if time[0] == "00":
@@ -62,7 +71,9 @@ def respond(voice_data):
             hours = time[0]
         minutes = time[1]
         time = hours + " hours and " + minutes + "minutes"
-        engine_speak(time)
+        monday_speak(time)
+    if there_exists(['shut down', 'exit', 'power down', 'initiate shutdown']):
+        shutdown()
 
 
 def archive_contract(*args, **kwargs):
@@ -92,11 +103,19 @@ def access_config_file():
         changed in the event of a move or hardware/software revamp 
         e.g. Local/Public IP, SSH config, Home Server connection settings
     """  
-
+def shutdown():
+    monday_speak('Initiating shutdown protocol.')
+    monday_speak('Deleting temporary audio files.')
+    for f in os.listdir():
+        if '.mp3' in f:
+            os.remove(f)
+    monday_speak('ArchIving logs')
+    monday_speak('Good day sir. ')
+    exit()
 
 time.sleep(1)
-engine = pyttsx3.init()
+# engine = pyttsx3.init()
 
-while(1):
-    voice_data = record_audio('Recording')
+while 1:
+    voice_data = record_audio()
     respond(voice_data)
